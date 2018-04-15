c12eed87d900c82b4aebc9eb6c3b0ddc43e8b892
SpacePlant <SpacePlant@users.noreply.github.com>
2018-04-16 01:29:56 +0200

    Added comments.

--- plankton/lcg/lcgtruncated.py
+++ plankton/lcg/lcgtruncated.py
@@ -13,13 +13,16 @@ class LCGTruncated(LCG):
     def recover(self, vals):
         self._verify_input(vals)
 
+        # Brute-force the truncated bits.
         truncated_bits = self._get_truncated_bits()
         reguired_values = self.get_info().req_vals
         for i in range(2**truncated_bits):
             self._state = (vals[0] << truncated_bits) | i
             for val in vals[1:reguired_values]:
+                # Candidate did not match.
                 if self.next() != val:
                     break
+            # Candidate did match.
             else:
                 self._verify_output(vals[reguired_values:])
                 return
